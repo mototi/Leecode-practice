a927a23be341851c3340962c0e82a7bd91cf591a
mohamed <moabulrahman.do@gmail.com>
2024-07-19 17:37:30 +0300

    enhance find-d nums solution

--- Find the Duplicate Number/solution.py
+++ Find the Duplicate Number/solution.py
@@ -4,23 +4,13 @@ def findDuplicate(nums) -> bool:
     :type nums: List[int]
     :rtype: bool
     """
-    temp_arr = []
     
-    biggest_num = 0
-
-    for n in nums:
-        if n > biggest_num:
-            biggest_num = n
-    
-    for n in range(biggest_num+1):
-        temp_arr.append(0)
+    temp_obj = {}
     
     for n in nums:
-        temp_arr[n] += 1
-
-    for n in  temp_arr:
-        if n > 1 :
+        if n in temp_obj:
             return True
+        temp_obj[n] = 1
         
     return False
 
